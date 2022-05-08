1edfa58f149276d74eca6db0828a6cea3579849c
bennetr <76742318+bennetrr@users.noreply.github.com>
2022-05-08 19:08:18 +0200

    Added docker compose

--- setup.sh
+++ setup.sh
@@ -173,7 +173,7 @@ if $docker; then
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$os_distributor $os_codename stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
         sudo apt update
-        sudo apt install -y docker-ce docker-ce-cli containerd.io
+        sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
     fi
 
     sudo usermod -aG docker "$USER"
